f7fcde332a31893c1c8e7cf10d2feaf9b2301e19
Phan Tuoi <phantuoi1xa@gmail.com>
2025-09-03 02:05:00 +0700

    api thuc don

--- controllers/mealSuggestionController.js
+++ controllers/mealSuggestionController.js
@@ -18,7 +18,7 @@ exports.getTodayMealSuggestions = async (req, res) => {
     const suggestions = recipes.filter(recipe => {
       const recipeIngredients = recipe.ingredients.map(i => i.name.toLowerCase());
       const matchedCount = recipeIngredients.filter(i => foodNames.includes(i)).length;
-      return matchedCount >= 1; // ít nhất 2 nguyên liệu
+      return matchedCount >= 4; // ít nhất 4 nguyên liệu
     });
 
     res.json({ suggestions }); // trả về toàn bộ thông tin món
@@ -27,3 +27,47 @@ exports.getTodayMealSuggestions = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+// -------------------- Lấy tất cả món theo category --------------------
+exports.getRecipesByCategory = async (req, res) => {
+  try {
+    const category = req.params.category;
+
+    // 1. Lấy tất cả công thức
+    const recipes = await Recipe.find({ category: category });
+
+    // 2. Trả về danh sách
+    res.json({ recipes });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// -------------------- Lấy tất cả món ăn --------------------
+exports.getAllRecipes = async (req, res) => {
+  try {
+    // Lấy toàn bộ công thức từ DB
+    const recipes = await Recipe.find();
+
+    // Trả về danh sách
+    res.json({ recipes });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// -------------------- Lấy tất cả category --------------------
+exports.getAllCategories = async (req, res) => {
+  try {
+    // Dùng distinct để lấy ra các category duy nhất
+    const categories = await Recipe.distinct("category");
+
+    res.json({ categories });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: err.message });
+  }
+};
+
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const authRoutes = require("./routes/authRoutes.js");
 const userRoutes = require("./routes/userRoutes.js");
 const recipesRoutes = require("./routes/recipesRoutes.js");
 const mealSuggestionRoutes = require("./routes/mealSuggestionRoutes.js");
+const  getRecipesByCategory  = require("./controllers/mealSuggestionController.js");
 
 const app = express();
 app.use(bodyParser.json());
@@ -34,6 +35,9 @@ app.use("/api/auths", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/recipes", recipesRoutes);
 app.use("/api/meals", mealSuggestionRoutes);
+app.get("/api/meals/category/:category", mealSuggestionRoutes);
+app.get("/api/meals/all", mealSuggestionRoutes);
+app.get("/api/meals/categories", mealSuggestionRoutes);
 
 // Chạy server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
